8187506211b8a1e3ff15315b6d02a97331c1257f
zainabshakeel <xainikhan20@gmail.com>
2023-04-07 15:14:10 +0500

    create item test

--- src/screens/AddItems.js
+++ src/screens/AddItems.js
@@ -1,13 +1,13 @@
-import { upload } from "@testing-library/user-event/dist/upload";
 import React, { useEffect, useState } from "react";
 
 import { ApiCall } from "../services/ApiCall";
 import API from "../services/ApiLists";
-
-var file = require("file-system");
-var fs = require("fs");
+import { useNavigate } from "react-router-dom";
+// var file = require("file-system");
+// var fs = require("fs");
 
 export default function AddItems() {
+  const navigate = useNavigate();
   const [name, setName] = useState();
   const [price, setPrice] = useState();
   const [image, setImage] = useState();
@@ -16,57 +16,82 @@ export default function AddItems() {
     var formData = {
       name,
       price,
-      img: "./img/test2.jpeg",
+      img: "./img/" + image,
     };
-
+    console.log("formData", formData);
     ApiCall("Post", API.items, formData)
       .catch((error) => {
         console.log("erorr reponse", error);
       })
-      .then((resp) => {});
+      .then((resp) => {
+        console.log("cteated", resp);
+        navigate("/items");
+      });
   }
 
-  const submitHandler = (event, files) => {
-    console.log(event.target);
-    console.log(files.target);
-    fs.writeFile(
-      `../components/img/${files.target.files[0].name}.jpg`,
-      files.target.files[0],
-      function (err) {
-        if (err) throw err;
-        console.log("Replaced!");
-      }
-    );
-  };
+  // const submitHandler = (event, files) => {
+  //   console.log(event.target);
+  //   console.log(files.target);
+  //   fs.writeFile(
+  //     `../components/img/${files.target.files[0].name}.jpg`,
+  //     files.target.files[0],
+  //     function (err) {
+  //       if (err) throw err;
+  //       console.log("Replaced!");
+  //     }
+  //   );
+  // };
 
   return (
     <div className="App">
-      <form className="m-5">
+      <div className="m-5">
         <div className="form-group">
           <p>Name</p>
           <input
             className="form-control"
             placeholder="Enter Product Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
         <div className="form-group">
           <p>Price</p>
-          <input className="form-control" placeholder="Price" required />
+          <input
+            className="form-control"
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
+          />
         </div>
         <div className="form-group">
+          <p>Product Image</p>
+          <input
+            className="form-control"
+            placeholder="Image name"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            required
+          />
+        </div>
+        {/* <div className="form-group">
           <p>Product Image</p>
           <input
             type="file"
             className="form-control-file"
             onChange={() => submitHandler()}
           />
-        </div>
+        </div> */}
 
-        <button type="submit" className="btn btn-warning">
+        <button
+          type="submit"
+          className="btn btn-warning mt-3"
+          onClick={() => AddItemApi()}
+        >
           Submit
         </button>
-      </form>
+      </div>
     </div>
   );
 }
